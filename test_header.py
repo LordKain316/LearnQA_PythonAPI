8e6a853bf10e2d6f50535ff8ff5afb59c8322bcc
Максим Кочетов <kochetov.ma@unicom24.ru>
2024-07-15 20:13:11 +0400

    test_header

---
+++ test_header.py
@@ -0,0 +1,29 @@
+import requests
+
+class TestHeaders:
+    def test_header(self):
+        url = "https://playground.learnqa.ru/api/homework_header"
+        a = {"header": "11111222223333344444"}
+        response = requests.get(url, a)
+
+        print(response)
+        assert response.status_code == 200, "Wrong response status"
+
+        header = response.headers
+        print(header)
+
+        secret_key = header.get('x-secret-homework-header')
+        print(secret_key)
+
+        expected_key = 'Some secret value'
+        assert secret_key == expected_key, "Not expected response secret-key"
+
+        content_type = header.get('Content-Type')
+        print(content_type)
+        expected_type = 'application/json'
+        assert content_type == expected_type, "Not expected content-type"
+
+        connection = header.get('Connection')
+        print(connection)
+        expected_connection = 'keep-alive'
+        assert connection == expected_connection, "Not expected connection"
