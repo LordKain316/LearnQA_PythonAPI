d24d028e77c80f9efa5d2eb67b1b84601430f760
Максим Кочетов <kochetov.ma@unicom24.ru>
2024-07-26 17:09:55 +0400

    test_user_get

---
+++ tests/test_user_get.py
@@ -0,0 +1,47 @@
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+class TestUserGet(BaseCase):
+    def setup_method(self):
+        self.data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+    def test_get_user_details_not_auth(self):
+        response = MyRequests.get("/user/2")
+
+        Assertions.assert_json_has_key(response, "username")
+        Assertions.assert_json_has_not_key(response, "email")
+        Assertions.assert_json_has_not_key(response, "firstName")
+        Assertions.assert_json_has_not_key(response, "lastName")
+
+    def test_get_user_details_auth_as_same_user(self):
+
+        response1 = MyRequests.post("/user/login/", data=self.data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        response2= MyRequests.get(f"/user/{user_id_from_auth_method}",
+                                header={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
+
+        expected_fields = ["username", "email", "firstName", "lastName"]
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_different_user(self):
+        response1 = MyRequests.post("/user/login/", data=self.data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        response2 = MyRequests.get(f"/user/101623",
+            header={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_code_status(response2, 200), f"Unexpected status code: '{response2.status_code}'"
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
